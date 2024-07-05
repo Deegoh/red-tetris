3cc925192a7235579d3ef8533b27f6da0ed2753e
Tiago <tiago.pintomonteiro@liip.ch>
2024-07-05 19:07:58 +0200

    Lean front setup

--- data/frontend/vite.config.js
+++ data/frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0'
-  },
-  preview: {
-    port: 8081,
-    host: '0.0.0.0'
-  },
   plugins: [react()],
 })
