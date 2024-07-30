26ef774cfc46bff336c3183590ef93c243fded28
Tiago <tiago.pintomonteiro@liip.ch>
2024-07-30 11:24:05 +0200

    Display tetromino on preview

--- data/frontend/src/components/Game.jsx
+++ data/frontend/src/components/Game.jsx
@@ -1,10 +1,11 @@
 import {Board} from "./tetris/Board.jsx";
-import {PreviewBlock} from "./tetris/PreviewBlock.jsx";
+import {PreviewTetrominoes} from "./tetris/PreviewBlock.jsx";
 import {Score} from "./tetris/Score.jsx";
 import {ControlsStore} from "./tetris/ControlsStore.jsx";
 import {Btn} from "./Btn.jsx";
 import {useDispatch, useSelector} from "react-redux";
 import {playAction, restartAction} from "../features/game/gameActions.js";
+import {tetrominoesBlocks} from "./tetris/tetrominoes.js";
 
 export const Game = () => {
   const dispatch = useDispatch();
@@ -21,10 +22,11 @@ export const Game = () => {
         }}>Restart</Btn>
         <Btn onClick={() => {
           console.log(game);
+          console.log(tetrominoesBlocks);
         }}>view selectorStore</Btn>
       </div>
       <Board/>
-      <PreviewBlock/>
+      <PreviewTetrominoes/>
       <ControlsStore/>
       {/* TODO: add message alert like countdown or whatever*/}
     </div>
--- data/frontend/src/components/tetris/PreviewBlock.jsx
+++ data/frontend/src/components/tetris/PreviewBlock.jsx
@@ -1,12 +1,28 @@
 import {Square} from "./Square.jsx";
+import {emptyColor, tetrominoes, tetrominoesBlocks} from "./tetrominoes.js";
 
-export const PreviewBlock = () => {
+export const randomTetromino = () => {
+  return tetrominoes.charAt(Math.floor(Math.random() * tetrominoes.length));
+}
+
+const parseAndFormatTetromino = (shape, rotation) => {
   const size = 16;
-  const previewBlock = [];
+  const tetromino = [];
+
+  if (!shape) {
+    shape = "default";
+  }
 
   for (let index = 0; index < size; index++) {
-    previewBlock.push(<Square position={index} key={index} color={'bg-tile'}/>);
+    let colorId = tetrominoesBlocks[shape][rotation][Math.floor(index / 4)][index % 4];
+    let color = colorId !== emptyColor ? "bg-tile-" + colorId : "bg-tile";
+    tetromino.push(<Square position={index} key={index} color={color}/>);
   }
+  return tetromino;
+}
+
+export const PreviewTetrominoes = ({shape, rotation = 0}) => {
+  const previewBlock = parseAndFormatTetromino(shape, rotation);
 
   return (
     <div className="preview-block grid grid-cols-4 gap-0 content-start">
