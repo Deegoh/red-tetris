a6ecd975e011f7fc05ee1baa0874de8e0c9c0cb6
Tiago <tiago.pintomonteiro@liip.ch>
2024-08-09 14:43:33 +0200

    Improve rendering system and add some cache

--- data/frontend/src/components/tetris/PreviewBlock.jsx
+++ data/frontend/src/components/tetris/PreviewBlock.jsx
@@ -1,26 +1,25 @@
 import { Square } from './Square.jsx';
 import { useSelector } from 'react-redux';
-
-const parseAndFormatTetromino = (shape) => {
-  const size = 16;
-  const tetromino = [];
-
-  if (shape === undefined) {
-    return;
-  }
-
-  for (let index = 0; index < size; index++) {
-    let colorId = shape[Math.floor(index / 4)][index % 4];
-    tetromino.push(
-      <Square mode={'player'} position={index} key={index} color={colorId} />
-    );
-  }
-  return tetromino;
-};
+import { useMemo } from 'react';
 
 export const PreviewTetrominoes = () => {
-  const previewTermino = useSelector((state) => state.game.previewTermino);
-  const previewBlock = parseAndFormatTetromino(previewTermino);
+  const previewTetromino = useSelector((state) => state.game.previewTetromino);
+  const previewBlock = useMemo(() => {
+    const size = 16;
+    const tetromino = [];
+
+    if (previewTetromino === undefined) {
+      return;
+    }
+
+    for (let index = 0; index < size; index++) {
+      let colorId = previewTetromino[Math.floor(index / 4)][index % 4];
+      tetromino.push(
+        <Square mode={'player'} position={index} key={index} color={colorId} />
+      );
+    }
+    return tetromino;
+  }, [previewTetromino]);
 
   return (
     <div className='bg-board p-1 rounded shrink-1 preview-block grid grid-cols-4 gap-[1px] content-start'>
