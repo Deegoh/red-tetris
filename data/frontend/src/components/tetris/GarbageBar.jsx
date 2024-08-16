95bcceb98f9f7f1cc520cd95b71b5fa815f41181
Deegoh <tiagopintomonteiro@gmail.com>
2024-08-16 18:45:25 +0200

    Readjust GarbageBar Size

--- data/frontend/src/components/tetris/GarbageBar.jsx
+++ data/frontend/src/components/tetris/GarbageBar.jsx
@@ -1,10 +1,36 @@
 import { useSelector } from 'react-redux';
+import useBreakpoint from './useBreakpoint.jsx';
 
 export const GarbageBar = () => {
   const garbageValue = useSelector((state) => state.game.incomingGarbage);
+  const screen = useBreakpoint();
+
+  const resize = () => {
+    let sizeTile = 35;
+
+    switch (screen) {
+      case 'xs':
+      case 'sm':
+        sizeTile = 20;
+        break;
+      case 'md':
+        sizeTile = 30;
+        break;
+    }
+
+    return sizeTile;
+  };
+
+  const size = resize();
+
+  const style = {
+    width: `${size}px`,
+  };
 
   return (
-    <div className='bg-board rounded-full h-full w-4 md:w-8 p-1 content-end place-self-end'>
+    <div
+      style={style}
+      className='bg-board rounded-full h-full p-1 content-end place-self-end'>
       <div
         style={{ height: `${garbageValue * 5}%` }}
         className={`shadow-ghost shadow-tile-W/35 bg-tile-W w-full rounded-full mx-auto`}
